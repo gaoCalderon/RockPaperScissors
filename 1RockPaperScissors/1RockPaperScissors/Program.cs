49fadbcdaa1b2b84eb146eca900f590bb7946471
gaoCalderon <gao.vang86@gmail.com>
2016-08-11 13:46:07 -0500

    initial

---
+++ 1RockPaperScissors/1RockPaperScissors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            game Game = new game();
+            Game.run();
+            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1RockPaperScissors/1RockPaperScissors/player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1RockPaperScissors
+{
+    class player
+    {
+        string name;
+
+        public player(string name)
+        {
+            this.name = name;
+        }
+    }
+}
